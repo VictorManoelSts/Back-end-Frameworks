a4bb3d7fccea1f02c234976bf2ec795dfbf010e8
Victor Manoel <victormanoelsantos109@gmail.com>
2026-09-15 10:22:02 -0300

    Model e conversão JSON

---
+++ api-cursos/src/main/java/br/edu/nassau/api_cursos/model/Curso.java
@@ -0,0 +1,41 @@
+﻿package br.edu.nassau.api_cursos.model;
+
+public class Curso {
+    private Long id;
+    private String nome;
+    private Integer cargaHoraria;
+
+    public Curso(){
+
+    }
+
+    public Curso(Long id, Integer cargaHoraria, String nome) {
+        this.id = id;
+        this.cargaHoraria = cargaHoraria;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
