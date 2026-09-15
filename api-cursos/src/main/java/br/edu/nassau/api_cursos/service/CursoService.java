7bb82cee4b71671517c1da8be9dd14a897663bd6
Victor Manoel <victormanoelsantos109@gmail.com>
2026-09-15 11:34:59 -0300

    Regras no Service

--- api-cursos/src/main/java/br/edu/nassau/api_cursos/service/CursoService.java
+++ api-cursos/src/main/java/br/edu/nassau/api_cursos/service/CursoService.java
@@ -1,6 +1,7 @@
 package br.edu.nassau.api_cursos.service;
 
 import br.edu.nassau.api_cursos.model.Curso;
+import br.edu.nassau.api_cursos.repository.CursoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,35 +9,40 @@ import java.util.List;
 
 @Service
 public class CursoService {
-    private final List<Curso> cursos = new ArrayList<>();
-    private Long proximoId= 1L;
+    private final CursoRepository repository;
 
-    public List<Curso> listar(){
-        return cursos;
+    public CursoService(CursoRepository repository){
+        this.repository = repository;
     }
 
     public Curso cadastrar(Curso curso){
-        curso.setId(proximoId++);
-        cursos.add(curso);
-        return curso;
+        if (curso.getNome() == null){
+            throw new IllegalArgumentException("O nome é obrigatório!");
+        }
+        if (curso.getCargaHoraria() <= 0){
+            throw new IllegalArgumentException("A carga Horária deve sr maior que 0!");
+        }
+        return repository.cadastrar(curso);
+    }
+    public List<Curso> listar(){
+        return repository.listar();
     }
     public Curso buscarPorId(Long id){
-        for (Curso curso : cursos){
-            if (curso.getId().equals(id)){
-                return curso;
-            }
-        }
-        return null;
+        return repository.buscarPorId(id);
     }
     public Curso atualizar(Long id, Curso dados){
-        Curso curso = buscarPorId(id);
-        if (curso == null) return  null;
+        Curso curso = repository.buscarPorId(id);
+        if (curso == null) return null;
         curso.setNome(dados.getNome());
         curso.setCargaHoraria(dados.getCargaHoraria());
         return curso;
     }
     public void remover(Long id){
-        Curso curso = buscarPorId(id);
-        cursos.remove(curso);
+        Curso curso = repository.buscarPorId(id);
+        if (curso != null){
+            repository.remover(curso);
+        }
+
     }
+
 }
